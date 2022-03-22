db35e7024eff83c191b0eb3bc61cfde416910dc6
Mitulsharma123 <sharmamitul72@gmail.com>
2022-03-22 15:25:28 +0530

    fix the bug in code

---
+++ bonus/main.go
@@ -0,0 +1,18 @@
+// Fix the Bug in code and make it working to print all values 
+
+package main_1
+
+func main(){
+	m := map[string]int{
+		"Tesla":"elon",
+		"facebook":"mark",
+		"apple":"tim",
+		"google":"sundar"
+	}
+
+	for _,value:=range map{
+		fmt.println(Value)
+	}
+		}
+	}
+}
